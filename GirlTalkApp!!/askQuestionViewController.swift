852c940ee177d19c72c027a724cde40b7ca52afd
Mia Alvarado <mealvarado17224@gmail.com>
2020-07-16 10:59:31 -0700

    Additions to base of app

---
+++ GirlTalkApp!!/askQuestionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  askQuestionViewController.swift
+//  GirlTalkApp!!
+//
+//  Created by Mia Elizabeth Alvarado on 7/16/20.
+//  Copyright © 2020 Zoe Howes. All rights reserved.
+//
+
+import UIKit
+
+class askQuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
